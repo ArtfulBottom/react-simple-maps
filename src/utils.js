ceb5149f1cc9b541c5ecfa8a265291ba606ac4db
Luke S <snyderluke8@gmail.com>
2020-06-07 16:40:01 -0400

    Update.

--- src/utils.js
+++ src/utils.js
@@ -21,11 +21,19 @@ export function fetchGeographies(url) {
 
 export function getFeatures(geographies, parseGeographies) {
   if (Array.isArray(geographies)) return parseGeographies ? parseGeographies(geographies) : geographies
+  // const feats = feature(
+  //   geographies,
+  //   geographies.objects[Object.keys(geographies.objects)[0]]
+  // ).features
+  // console.log(feats);
   const feats = feature(
     geographies,
     geographies.objects[Object.keys(geographies.objects)[0]]
   ).features
-  console.log(feats);
+  console.log(geographies.objects);
+  console.log(geographies);
+  console.log(geographies.objects[Object.keys(geographies.objects)[0]])
+
   return parseGeographies ? parseGeographies(feats) : feats
 }
 
